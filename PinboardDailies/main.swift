441e0d67469183bcdd358ebda1ca8e2ee19262d4
Teo <m@sartori.dk>
2014-07-04 15:15:33 +0200

    PinboardDailies is an OS X command line utility to fetch the bookmarks tagged with daily from Pinboard and return the results in the xml format that Alfred can parse into a results list.
    The usage is:
    
    ./PinboardDailies tag: "tagName" token: "Your Pinboard token"
    
    In Alfred's Workflows you simply add a Script Filter with the above (using your own credentials of course) and flow the output into an Open URL Action.

---
+++ PinboardDailies/main.swift
@@ -0,0 +1,98 @@
+//
+//  main.swift
+//  PinboardDailies
+//
+//  Created by Matteo Sartori on 03/06/14.
+//  Copyright (c) 2014 Teo Sartori. All rights reserved.
+//
+
+import Foundation
+
+
+class PinboardToAlfred {
+    
+    func fetchBookmarks(#tag: String, token: String) {
+        let url = NSURL.URLWithString("https://api.pinboard.in/v1/posts/all?auth_token=\(token)&tag=\(tag)&format=json")
+        let request = NSURLRequest(URL: url)
+        let queue = NSOperationQueue()
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: {
+            (response, data, error) -> Void in
+
+            if error != nil {
+                println("Much error. Great bye")
+                println(error)
+            } else {
+                
+                let json: NSArray = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSArray
+                
+                var rootXML = NSXMLElement(name: "items")
+                
+                for entry: AnyObject in json {
+                    
+                    var description     = entry.objectForKey("description") as String
+                    var href            = entry.objectForKey("href") as String
+                    var childXML        = NSXMLElement(name: "item")
+                    
+                    childXML.addAttribute(NSXMLNode.attributeWithName("arg", stringValue: href) as NSXMLNode)
+                    childXML.addAttribute(NSXMLNode.attributeWithName("valid", stringValue: "YES") as NSXMLNode)
+                    childXML.addAttribute(NSXMLNode.attributeWithName("type", stringValue: "file") as NSXMLNode)
+                    
+                    // Add the child to the root.
+                    rootXML.addChild(childXML)
+                    
+                    var subChildXML = NSXMLElement(name: "subtitle", stringValue: "test")
+                    childXML.addChild(subChildXML)
+                    
+                    subChildXML = NSXMLElement(name: "icon")
+                    subChildXML.addAttribute(NSXMLNode.attributeWithName("type", stringValue: "fileicon") as NSXMLNode)
+                    childXML.addChild(subChildXML)
+                    
+                    subChildXML = NSXMLElement(name: "title", stringValue: description)
+                    childXML.addChild(subChildXML)
+                    
+                }
+                
+                var alfredDoc = NSXMLDocument(rootElement: rootXML)
+                alfredDoc.version = "1.0"
+                alfredDoc.characterEncoding = "UTF-8"
+                
+                println(alfredDoc.XMLString)
+                
+                // Write it out to disk
+                //alfredDoc.XMLData.writeToFile("/Users/teo/tmp/test.xml", atomically: true)
+                exit(0)
+            }
+        })
+    }
+    
+    func runRun() {
+        let runLoop = NSRunLoop.currentRunLoop()
+        let main = PinboardToAlfred()
+        
+        var tag = "daily"
+        var token: String?
+        
+        var index = 1
+        while index+1 < Int(C_ARGC) {
+            switch (String.fromCString(C_ARGV[index++]), String.fromCString(C_ARGV[index++])) {
+            case let("tag:",value):
+                tag = value
+            case let("token:",value):
+                token = value
+            default:
+                break
+            }
+        }
+        
+        if token {
+            main.fetchBookmarks(tag: tag, token: token!)
+            runLoop.run()
+        } else {
+            println("Error! No valid token provided.")
+            println("Usage: PinboardDailies tag: \"daily\" token: \"username:tokendata\"")
+        }
+    }
+}
+
+PinboardToAlfred().runRun()
