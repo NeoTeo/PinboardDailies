dc26358751d3c7c56dcc7ff88ab605aa924bd7b1
Teo Sartori <m@sartori.dk>
2015-10-07 13:02:33 +0200

    Replace deprecated async call. Pull functions out of class.

--- PinboardDailies/main.swift
+++ PinboardDailies/main.swift
@@ -3,140 +3,137 @@
 //  PinboardDailies
 //
 //  Created by Matteo Sartori on 03/06/14.
-//  Copyright (c) 2014 Teo Sartori. All rights reserved.
+//  Licensed under MIT See LICENCE file in the root of this project for details.
 //
 
 import Foundation
 
+enum FetchMode: Int {
+    case display = 0
+    case silent
+}
 
-class PinboardToAlfred {
-    
-    enum FetchMode: Int {
-        case display = 0
-        case silent
-    }
-    
-    func fetchBookmarks(tag tag: String, token: String, mode: FetchMode ) {
+func fetchBookmarks(tag tag: String, token: String, mode: FetchMode ) {
 
-        let url     = NSURL(string: "https://api.pinboard.in/v1/posts/all?auth_token=\(token)&tag=\(tag)&format=json")
-        let request = NSURLRequest(URL: url!)
-        let queue   = NSOperationQueue()
-        
-        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: {
-            (response: NSURLResponse?, data: NSData?, urlError: NSError?) -> Void in
-            
-            if urlError != nil {
-                print("Much error. Great bye")
-                NSLog("ERROR: %@",urlError!)
-                exit(-1)
-            }
+    let url     = NSURL(string: "https://api.pinboard.in/v1/posts/all?auth_token=\(token)&tag=\(tag)&format=json")
+    let request = NSURLRequest(URL: url!)
 
-            // Parse the data into an array of string : anyobject dictionaries.
-            let json    = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? [[String : AnyObject]]
-            let rootXML = NSXMLElement(name: "items")
-            
-            // traverse the data and turn it into an XML format Alfred understands.
-            for entry in json! {
-                if let
-                    description     = entry["description"] as? String,
-                    href            = entry["href"] as? String
-                {
-                    let childXML = NSXMLElement(name: "item")
-                    
-                    childXML.addAttribute(NSXMLNode.attributeWithName("arg"     , stringValue:   href) as! NSXMLNode)
-                    childXML.addAttribute(NSXMLNode.attributeWithName("valid"   , stringValue:  "YES") as! NSXMLNode)
-                    childXML.addAttribute(NSXMLNode.attributeWithName("type"    , stringValue: "file") as! NSXMLNode)
-                    
-                    // Add the child to the root.
-                    rootXML.addChild(childXML)
-                    
-                    var subChildXML = NSXMLElement(name: "subtitle", stringValue: href)
-                    childXML.addChild(subChildXML)
-                    
-                    
-                    subChildXML = NSXMLElement(name: "icon")
-                    subChildXML.addAttribute(NSXMLNode.attributeWithName("type", stringValue: "fileicon") as! NSXMLNode)
-                    childXML.addChild(subChildXML)
-                
-                    
-                    subChildXML = NSXMLElement(name: "title", stringValue: description)
-                    childXML.addChild(subChildXML)
-                }
-                
-            }
-            
-            let alfredDoc               = NSXMLDocument(rootElement: rootXML)
-            alfredDoc.version           = "1.0"
-            alfredDoc.characterEncoding = "UTF-8"
-            
-            if mode == .display {
-                print(alfredDoc.XMLString)
-            }
-            
-            // Write it out to disk (just the dailies for now)
-            if tag == "daily" {
-                alfredDoc.XMLData.writeToFile("/Users/teo/tmp/cachedDailiesXML.xml", atomically: true)
-            }
-            exit(0)
+    let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+        (data: NSData?, response: NSURLResponse?, urlError: NSError?) -> Void in
         
-        })
-    }
-    
-    func checkForCachedXML(cachedXMLURL: NSURL) -> NSXMLDocument? {
-        
-        do {
-            let cachedXML: NSXMLDocument? = try NSXMLDocument(contentsOfURL: cachedXMLURL, options: Int(NSXMLDocumentContentKind.XMLKind.rawValue))
-            return cachedXML
-            
-        } catch {
-            print(error)
-            return nil
-        }
-    }
-    
-    func runRun() {
-
-        var userToken: String?
-        let main     = PinboardToAlfred()
-        var userTag  = "daily"
-        let argArray = Process.arguments as [String]
-        
-        if argArray.count < 3 { exit(0) }
-        
-        // Skip the first index as it is always the application name.
-        for index in 1.stride(through: argArray.count-1, by: 2) {
-            switch (argArray[index], argArray[index+1]) {
-
-                case ("tag:", let value):
-                    userTag = value
-                
-                case ("token:", let value):
-                    userToken = value
-                
-                default:
-                    break
-            }
+        if urlError != nil {
+            print("Much error. Great bye")
+            NSLog("ERROR: %@",urlError!)
+            exit(-1)
         }
 
-        if userToken != nil {
-            // By default the fetchBookmarks display the bookmarks it finds
-            var fetchMode = FetchMode.display
+        // Parse the data into an array of string : anyobject dictionaries.
+        let json    = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? [[String : AnyObject]]
+        let rootXML = NSXMLElement(name: "items")
+        
+        // traverse the data and turn it into an XML format Alfred understands.
+        for entry in json! {
+            if let
+                description     = entry["description"] as? String,
+                href            = entry["href"] as? String
+            {
+                let childXML = NSXMLElement(name: "item")
+                
+                childXML.addAttribute(NSXMLNode.attributeWithName("arg"     , stringValue:   href) as! NSXMLNode)
+                childXML.addAttribute(NSXMLNode.attributeWithName("valid"   , stringValue:  "YES") as! NSXMLNode)
+                childXML.addAttribute(NSXMLNode.attributeWithName("type"    , stringValue: "file") as! NSXMLNode)
+                
+                // Add the child to the root.
+                rootXML.addChild(childXML)
+                
+                var subChildXML = NSXMLElement(name: "subtitle", stringValue: href)
+                childXML.addChild(subChildXML)
+                
+                
+                subChildXML = NSXMLElement(name: "icon")
+                subChildXML.addAttribute(NSXMLNode.attributeWithName("type", stringValue: "fileicon") as! NSXMLNode)
+                childXML.addChild(subChildXML)
             
-            // But if we already have a cache, there's no need to display the bookmarks we fetch below.
-            if userTag.lowercaseString == "daily",
-                let cachedXML = checkForCachedXML(NSURL(fileURLWithPath: "/Users/teo/tmp/cachedDailiesXML.xml")) {
-                    print(cachedXML)
-                    fetchMode = .silent
+                
+                subChildXML = NSXMLElement(name: "title", stringValue: description)
+                childXML.addChild(subChildXML)
             }
             
-            main.fetchBookmarks(tag: userTag, token: userToken!, mode: fetchMode)
-            
-            CFRunLoopRun()
-        } else {
-            print("Error! No valid token provided.")
-            print("Usage: PinboardDailies tag: \"daily\" token: \"username:tokendata\"")
         }
+        
+        let alfredDoc               = NSXMLDocument(rootElement: rootXML)
+        alfredDoc.version           = "1.0"
+        alfredDoc.characterEncoding = "UTF-8"
+        
+        if mode == .display {
+            print(alfredDoc.XMLString)
+        }
+        
+        // Write it out to disk (just the dailies for now)
+        if tag == "daily" {
+            alfredDoc.XMLData.writeToFile("/Users/teo/tmp/cachedDailiesXML.xml", atomically: true)
+        }
+        exit(0)
+    
+    }
+    
+    task.resume()
+}
+
+func checkForCachedXML(cachedXMLURL: NSURL) -> NSXMLDocument? {
+    
+    do {
+        let cachedXML: NSXMLDocument? = try NSXMLDocument(contentsOfURL: cachedXMLURL, options: Int(NSXMLDocumentContentKind.XMLKind.rawValue))
+        return cachedXML
+        
+    } catch {
+        print(error)
+        return nil
     }
 }
 
-PinboardToAlfred().runRun()
+func runRun() {
+
+    var userToken: String?
+    var userTag  = "daily"
+    let argArray = Process.arguments as [String]
+    
+    if argArray.count < 3 { exit(0) }
+    
+    // Skip the first index as it is always the application name.
+    for index in 1.stride(through: argArray.count-1, by: 2) {
+        switch (argArray[index], argArray[index+1]) {
+
+            case ("tag:", let value):
+                userTag = value
+            
+            case ("token:", let value):
+                userToken = value
+            
+            default:
+                break
+        }
+    }
+
+    if userToken != nil {
+        // By default the fetchBookmarks display the bookmarks it finds
+        var fetchMode = FetchMode.display
+        
+        // But if we already have a cache, there's no need to display the bookmarks we fetch below.
+        if userTag.lowercaseString == "daily",
+            let cachedXML = checkForCachedXML(NSURL(fileURLWithPath: "/Users/teo/tmp/cachedDailiesXML.xml")) {
+                print(cachedXML)
+                fetchMode = .silent
+        }
+        
+
+        fetchBookmarks(tag: userTag, token: userToken!, mode: fetchMode)
+        
+        CFRunLoopRun()
+    } else {
+        print("Error! No valid token provided.")
+        print("Usage: PinboardDailies tag: \"daily\" token: \"username:tokendata\"")
+    }
+}
+
+runRun()
