ac0fa2232c5e118b826f4b05c7b8ac5339a9bafd
Teo Sartori <m@sartori.dk>
2016-06-14 10:00:10 +0200

    Update for latest Swift 3 WWDC release.

--- PinboardDailies/main.swift
+++ PinboardDailies/main.swift
@@ -13,55 +13,54 @@ enum FetchMode: Int {
     case silent
 }
 
-func fetchBookmarks(tag: String, token: String, mode: FetchMode ) {
+func fetchBookmarks(with tag: String, token: String, mode: FetchMode ) {
 
-    let url     = NSURL(string: "https://api.pinboard.in/v1/posts/all?auth_token=\(token)&tag=\(tag)&format=json")
-    let request = NSURLRequest(url: url!)
+    let url     = URL(string: "https://api.pinboard.in/v1/posts/all?auth_token=\(token)&tag=\(tag)&format=json")
+    let request = URLRequest(url: url!)
 
-    let task = NSURLSession.shared().dataTask(with: request) {
-        (data: NSData?, response: NSURLResponse?, urlError: NSError?) -> Void in
+    let task = URLSession.shared().dataTask(with: request) {
+        (data: Data?, response: URLResponse?, urlError: NSError?) -> Void in
         
-        if urlError != nil {
-            print("Much error. Great bye")
-            NSLog("ERROR: %@",urlError!)
+        guard urlError == nil else {
+            print("Much error. Great bye. \(urlError)")
             exit(-1)
         }
 
         // Parse the data into an array of string : anyobject dictionaries.
-        let json    = try! NSJSONSerialization.jsonObject(with: data!, options: NSJSONReadingOptions.mutableContainers) as? [[String : AnyObject]]
-        let rootXML = NSXMLElement(name: "items")
+        let json    = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? [[String : AnyObject]]
+        let rootXML = XMLElement(name: "items")
         
         // traverse the data and turn it into an XML format Alfred understands.
         for entry in json! {
-            if let
-                description     = entry["description"] as? String,
-                href            = entry["href"] as? String
-            {
-                let childXML = NSXMLElement(name: "item")
-                
-                childXML.addAttribute(NSXMLNode.attribute(withName: "arg"     , stringValue:   href) as! NSXMLNode)
-                childXML.addAttribute(NSXMLNode.attribute(withName: "valid"   , stringValue:  "YES") as! NSXMLNode)
-                childXML.addAttribute(NSXMLNode.attribute(withName: "type"    , stringValue: "file") as! NSXMLNode)
-                
-                // Add the child to the root.
-                rootXML.addChild(childXML)
-                
-                var subChildXML = NSXMLElement(name: "subtitle", stringValue: href)
-                childXML.addChild(subChildXML)
-                
-                
-                subChildXML = NSXMLElement(name: "icon")
-                subChildXML.addAttribute(NSXMLNode.attribute(withName: "type", stringValue: "fileicon") as! NSXMLNode)
-                childXML.addChild(subChildXML)
             
-                
-                subChildXML = NSXMLElement(name: "title", stringValue: description)
-                childXML.addChild(subChildXML)
-            }
+            guard let description = entry["description"] as? String, href = entry["href"] as? String
+            else { continue }
+            
+            let childXML = XMLElement(name: "item")
+            
+            childXML.addAttribute(XMLNode.attribute(withName: "arg"     , stringValue:   href) as! XMLNode)
+            childXML.addAttribute(XMLNode.attribute(withName: "valid"   , stringValue:  "YES") as! XMLNode)
+            childXML.addAttribute(XMLNode.attribute(withName: "type"    , stringValue: "file") as! XMLNode)
+            
+            // Add the child to the root.
+            rootXML.addChild(childXML)
+            
+            var subChildXML = XMLElement(name: "subtitle", stringValue: href)
+            childXML.addChild(subChildXML)
+            
+            
+            subChildXML = XMLElement(name: "icon")
+            subChildXML.addAttribute(XMLNode.attribute(withName: "type", stringValue: "fileicon") as! XMLNode)
+            childXML.addChild(subChildXML)
+        
+            
+            subChildXML = XMLElement(name: "title", stringValue: description)
+            childXML.addChild(subChildXML)
+            
             
         }
         
-        let alfredDoc               = NSXMLDocument(rootElement: rootXML)
+        let alfredDoc               = XMLDocument(rootElement: rootXML)
         alfredDoc.version           = "1.0"
         alfredDoc.characterEncoding = "UTF-8"
         
@@ -70,8 +69,12 @@ func fetchBookmarks(tag: String, token: String, mode: FetchMode ) {
         }
         
         // Write it out to disk (just the dailies for now)
-        if tag == "daily" {
-            alfredDoc.xmlData.write(toFile: "/tmp/cachedDailiesXML.xml", atomically: true)
+        if let url = URL(string: "file://"+NSHomeDirectory()+"/tmp/cachedDailiesXML.xml") where tag == "daily" {
+            do {
+                try alfredDoc.xmlData.write(to: url, options: NSData.WritingOptions.atomicWrite)
+            } catch {
+                print("Error \(error) writing XML data. Exiting.")
+            }
         }
         exit(0)
     
@@ -80,10 +83,10 @@ func fetchBookmarks(tag: String, token: String, mode: FetchMode ) {
     task.resume()
 }
 
-func checkForCachedXML(cachedXMLURL: NSURL) -> NSXMLDocument? {
+func checkForCachedXML(_ cachedXMLURL: URL) -> XMLDocument? {
     
     do {
-        let cachedXML: NSXMLDocument? = try NSXMLDocument(contentsOf: cachedXMLURL, options: Int(NSXMLDocumentContentKind.xmlKind.rawValue))
+        let cachedXML: XMLDocument? = try XMLDocument(contentsOf: cachedXMLURL, options: Int(XMLDocument.ContentKind.xml.rawValue))
         return cachedXML
         
     } catch {
@@ -121,13 +124,13 @@ func runRun() {
         
         // But if we already have a cache, there's no need to display the bookmarks we fetch below.
         if userTag.lowercased() == "daily",
-            let cachedXML = checkForCachedXML(cachedXMLURL: NSURL(fileURLWithPath: "/tmp/cachedDailiesXML.xml")) {
+            let cachedXML = checkForCachedXML(URL(fileURLWithPath: NSHomeDirectory()+"/tmp/cachedDailiesXML.xml")) {
                 print(cachedXML)
                 fetchMode = .silent
-        }
         
+        }
 
-        fetchBookmarks(tag: userTag, token: userToken!, mode: fetchMode)
+        fetchBookmarks(with: userTag, token: userToken!, mode: fetchMode)
         
         CFRunLoopRun()
     } else {
